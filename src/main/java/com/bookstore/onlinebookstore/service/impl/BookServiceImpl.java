eacb22c291ecb87bc5263553a18dcca50fa5ecae
maxima <maxima.at.work@gmail.com>
2026-05-27 11:51:17 +0300

    Implement Book entity, BookRepository and BookService

---
+++ src/main/java/com/bookstore/onlinebookstore/service/impl/BookServiceImpl.java
@@ -0,0 +1,28 @@
+package com.bookstore.onlinebookstore.service.impl;
+
+import com.bookstore.onlinebookstore.model.Book;
+import com.bookstore.onlinebookstore.repository.BookRepository;
+import com.bookstore.onlinebookstore.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
