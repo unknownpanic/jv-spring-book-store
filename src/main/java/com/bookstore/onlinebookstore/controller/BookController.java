08983c334179083ad5dd8caab090d466058df41a
maxima <maxima.at.work@gmail.com>
2026-06-04 00:04:15 +0300

    Change default api path & add http reponse status for postmapping

--- src/main/java/com/bookstore/onlinebookstore/controller/BookController.java
+++ src/main/java/com/bookstore/onlinebookstore/controller/BookController.java
@@ -5,16 +5,18 @@ import com.bookstore.onlinebookstore.model.dto.CreateBookRequestDto;
 import com.bookstore.onlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -29,6 +31,7 @@ public class BookController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDto createBook(@RequestBody CreateBookRequestDto createBookRequestDto) {
         return bookService.createBook(createBookRequestDto);
     }
