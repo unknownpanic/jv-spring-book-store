0a94079e37730fc3d09ac22190dd81787ed6444a
maxima <maxima.at.work@gmail.com>
2026-06-09 23:56:24 +0300

    Add data validation in CreateBookRequestDto & implement GlobalExceptionHandler

--- src/main/java/com/bookstore/onlinebookstore/model/dto/CreateBookRequestDto.java
+++ src/main/java/com/bookstore/onlinebookstore/model/dto/CreateBookRequestDto.java
@@ -1,5 +1,8 @@
 package com.bookstore.onlinebookstore.model.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,9 +10,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
+    @NotEmpty
     private String title;
+    @NotEmpty
     private String author;
+    @NotEmpty
     private String isbn;
+    @NotNull
+    @PositiveOrZero
     private BigDecimal price;
     private String description;
     private String coverImage;
