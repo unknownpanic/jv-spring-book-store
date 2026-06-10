2faa38aa519bcb56d0653a58581568537a64b45c
maxima <maxima.at.work@gmail.com>
2026-06-10 13:28:15 +0300

    Change validation strategy for strings to @NotBlank

--- src/main/java/com/bookstore/onlinebookstore/model/dto/CreateBookRequestDto.java
+++ src/main/java/com/bookstore/onlinebookstore/model/dto/CreateBookRequestDto.java
@@ -1,6 +1,6 @@
 package com.bookstore.onlinebookstore.model.dto;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
@@ -10,11 +10,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CreateBookRequestDto {
-    @NotEmpty
+    @NotBlank
     private String title;
-    @NotEmpty
+    @NotBlank
     private String author;
-    @NotEmpty
+    @NotBlank
     private String isbn;
     @NotNull
     @PositiveOrZero
